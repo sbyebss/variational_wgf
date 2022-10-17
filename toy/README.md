319b50cc490a58fe0b7c214711ce28f9a2738d52
sbyebss <sbyebss@gmail.com>
2022-10-17 01:20:04 -0400

    fix: clean gmm scripts

--- toy/README.md
+++ toy/README.md
@@ -4,7 +4,10 @@
 
 ```shell
 pip install --no-deps -r requirements.txt
+pip install torch==1.10.1+cu113 torchvision==0.11.2+cu113 -f https://download.pytorch.org/whl/cu113/torch_stable.html
 ```
+
+Replace the `gpus` in `configs/trainer/default.yaml` by the GPU id you want to use. For example, if you want to use GPU 3, then write `gpus: [3]`.
 
 ## Repository structure
 
--- toy/bash/gmm_reproduce.sh
+++ toy/bash/gmm_reproduce.sh
@@ -17,8 +17,4 @@ python run.py -m paper=gmm_d128 logger=wandb seed=1,2,3,4,5 logger.wandb.tags=\[
 
 #! test
 
-python run.py paper=gmm_d2 seed=1 mode=evaluation skip_train=True test_after_training=True
-
-# gmm_d2,gmm_d4,gmm_d8,gmm_d17,gmm_d24,gmm_d32,gmm_d64,gmm_d128
-# seed=1,2,3,4,5 
-python run.py -m paper=gmm_d128,gmm_d64,gmm_d2,gmm_d4,gmm_d8,gmm_d17,gmm_d24,gmm_d32 seed=1,2,3,4,5 mode=evaluation skip_train=True test_after_training=True
+# python run.py -m paper=gmm_d128,gmm_d64,gmm_d2,gmm_d4,gmm_d8,gmm_d17,gmm_d24,gmm_d32 seed=1,2,3,4,5 mode=evaluation skip_train=True test_after_training=True
